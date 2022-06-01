1715438e27616ef2e95762131e3d1b98981c1b34
jean-marc olivier-latapie <enguerrandol@gmail.com>
2022-06-01 09:19:32 +0200

    créer les entites

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,19 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
 
-// start the Stimulus application
-import './bootstrap';
+// or you can includes specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
+
+
+console.log('Hello webpack Encore !')
+
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -1,19 +1,36 @@
 <?php
-// src/Controller/ProgramController.php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/index', name: 'index')]
     public function index(): Response
     {
-        return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series',
+        $wild = '<h1> bonjour </h1>';
+
+        if (isset ($_GET['id'])) {
+            $id = intval($_GET['id']);
+            return $this->redirectToRoute('program_show', ['id' => $id]);
+        }
+        return $this->render('home/index.html.twig', [
+            'website' => $wild
         ]);
 
     }
+
+    #[Route('index/show/{id}', methods: ['GET'], name: 'show')]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'page' => $id,
+        ]);
+
+    }
+
 }
